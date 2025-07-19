d01e6d540b484f7f2d1d1c9ecaf115bed3c15b01
Zealina Hudson <zealinahudson@gmail.com>
2025-07-19 15:41:18 +0100

    handle Group Management

--- bot/handlers/unknown.py
+++ bot/handlers/unknown.py
@@ -8,6 +8,9 @@ from json import dumps
 
 async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """Prints out unknown commands"""
+    print("\n\nThis is from Unknown Handler\n\n")
     print(dumps(update.to_dict(), indent=2))
+    print("")
+    print(update)
 
 unknown_handler = MessageHandler(filters.ALL, unknown)
--- main.py
+++ main.py
@@ -4,15 +4,22 @@
 from telegram.ext import ApplicationBuilder
 from bot.utils.env import BOT_TOKEN
 from bot.handlers.start import start_handler
-from bot.handlers.chat_member import chat_member_handler
+from bot.handlers.group import chat_member_handler, list_groups, select_group
 from bot.handlers.unknown import unknown_handler
+from bot.handlers.persistence import persistence
+from bot.handlers.topic import topic_handler
+from bot.handlers.update_handler import update_handler
 
 
-application = ApplicationBuilder().token(BOT_TOKEN).build()
+application = ApplicationBuilder().token(BOT_TOKEN).persistence(persistence).build()
 
 application.add_handler(start_handler)
 application.add_handler(chat_member_handler)
+application.add_handler(list_groups)
+application.add_handler(select_group)
 application.add_handler(unknown_handler)
+application.add_handler(topic_handler)
+application.add_handler(update_handler)
 
 
 if __name__ == '__main__':
